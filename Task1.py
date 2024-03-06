7cdfb160e903b1d42d2f9ddb689eedb7584c44a8
Jame5M <95483317+Jame5M@users.noreply.github.com>
2024-03-06 16:18:38 +0000

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,77 @@
+import re
+from math import sqrt
+from pdfminer.high_level import extract_pages, extract_text
+#from matplotlib import pyplot as plt
+
+k=0.3062
+# takes in 2 arrays representing a pair of particles and returns energy and truncated energy
+def PairwiseEnergy(P1,P2):
+    numerator = P1[4]*P2[4]
+    denominator = sqrt((P1[1]-P2[1])**2+(P1[2]-P2[2])**2+(P1[3]-P2[3])**2)
+    energy=(numerator*k)/denominator
+    return(energy,round(energy))
+
+# Reads PDF file of the task sent via the email interview invitation and uses a Regular expression
+# search to extract particle data as strings.
+text=extract_text("Task1/TASK 1 Energy of a system of charged particles.pdf")
+print("here")
+DataPattern = re.compile(r"[0-9]+\s[+-][0-9][.]?[0-9]*\s[+-][0-9][.]?[0-9]*\s[+-][0-9][.]?[0-9]*\s[-\s][0-9]+")
+RawParticleData = DataPattern.findall(text)
+
+# each particle is a string and needs to be converted into an array of data to be used in calculations
+ParticleData = []
+### redundent code used to visualise the system ###
+# x=[]
+# y=[]
+# z=[]
+# m=[]
+for Particle in RawParticleData:
+    DataStringArray = Particle.split()
+    ParticleData.append([int(DataStringArray[0]),
+               float(DataStringArray[1]),
+               float(DataStringArray[2]),
+               float(DataStringArray[3]),
+               int(DataStringArray[4])])
+    ### redundent code used to visualise the system ###
+    # x.append(float(DataStringArray[1]))
+    # y.append(float(DataStringArray[2]))
+    # z.append(float(DataStringArray[3]))
+    # m.append(int(DataStringArray[4]))
+### redundent code used to visualise the system ###
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# img=ax.scatter(x,y, z, c=m,cmap='prism', alpha=1)
+# plt.show()
+
+TrueSystemEnergy=0
+EstimatedSystemEnergy=0
+PairwiseInteractions=0
+while ParticleData !=[]:
+    P1= ParticleData[0]
+    ParticleData.pop(0)
+    for Particle in ParticleData:
+        TrueEnergy,EstimatedEnergy=PairwiseEnergy(P1,Particle)
+        TrueSystemEnergy=TrueSystemEnergy+TrueEnergy
+        EstimatedSystemEnergy=EstimatedSystemEnergy+EstimatedEnergy
+        PairwiseInteractions += 1
+        
+
+Error=abs(((TrueSystemEnergy-EstimatedSystemEnergy)/TrueSystemEnergy)*100)
+print(Error)
+
+
+f=open("Task1Results","w")
+f.write("Actual System Energy: ")
+f.write(str(TrueSystemEnergy))
+f.write("\n")
+f.write("Estimated System Energy: ",)
+f.write(str(EstimatedSystemEnergy))
+f.write("\n")
+f.write("Error: ")
+f.write(str(Error))
+f.write("%")
+f.write("\n")
+f.write("Pairwise Interactions: ")
+f.write(str(PairwiseInteractions))
+f.write("\n")
+f.close()
---
+++ Task2.py
@@ -0,0 +1,72 @@
+def func(x):
+    return(4/(1+x**2))
+    
+def divisions(n):
+    div=1/n
+    yvals=[]
+    n=n+1
+    while n !=0:
+        n=n-1
+        x=1-(div*n)
+        yvals.append(func(x))
+
+    return(yvals,div)
+
+def TrapezoidRule(yvals,div):
+    i=0
+    a=0
+    while i < len(yvals)-1:
+        a=a+yvals[i]+ yvals[i+1]
+        i=i+1
+    Area=a*div/2
+    print("Trapezoid ",i, " Area: ", Area)
+    return (Area)
+    
+def SimpsonRule(yvals,div):
+    n=len(yvals)-1
+    Sum=0
+    for i , val in enumerate(yvals):
+        if i in (0,n):
+            Sum=Sum+val
+        elif i%2 == 0:
+            Sum=Sum+2*val
+        else:
+            Sum=Sum+4*val
+
+    Area=Sum*div/3
+    print("Simpson ",i," Area: ",Area)
+    return Area
+
+def error(Result):
+    return abs((1-(Result/3.1415667))*100)
+
+yvals,div=divisions(4)
+Result1=TrapezoidRule(yvals,div)
+#print("Error:",error(Result1),"%")
+
+Result2=SimpsonRule(yvals,div)
+#print("Error:",error(Result2),"%")
+
+yvals,div=divisions(12)
+Result3=TrapezoidRule(yvals,div)
+#print("Error:",error(Result3),"%")
+
+
+f=open("Task2Results.txt","w")
+f.write(("Method          N      Value of Pi     Error(%)\n"))
+f.write("Trapezoidal     4  ")
+f.write(str(Result1))
+f.write("  ")
+f.write(str(error(Result1)))
+f.write("\n")
+f.write("Simpson         4  ")
+f.write(str(Result2))
+f.write("  ")
+f.write(str(error(Result2)))
+f.write("\n")
+f.write("Trapezoidal    12  ")
+f.write(str(Result3))
+f.write(" ")
+f.write(str(error(Result3)))
+f.write("\n")
+f.close()
